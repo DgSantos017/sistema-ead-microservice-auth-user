b3b3266adc681ac5c86474f8555b102be21cdd52
Diogo <diogodevweb@gmail.com>
2023-07-26 00:08:18 -0300

    fix: ajust reposittory add interface fox extension

--- src/main/java/com/ead/authuser/repositories/UserRepository.java
+++ src/main/java/com/ead/authuser/repositories/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public class UserRepository extends JpaRepository<UserModel, UUID> {
+public interface UserRepository extends JpaRepository<UserModel, UUID> {
 }
