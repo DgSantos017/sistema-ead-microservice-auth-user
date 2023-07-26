e0fcdb5482201c90e93b31efd61e8bf97245b447
Diogo <diogodevweb@gmail.com>
2023-07-26 18:38:19 -0300

    feat: add filters advanceds with lib specifications

--- src/main/java/com/ead/authuser/services/implementation/UserServiceImplementation.java
+++ src/main/java/com/ead/authuser/services/implementation/UserServiceImplementation.java
@@ -5,6 +5,7 @@ import com.ead.authuser.repositories.UserRepository;
 import com.ead.authuser.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import org.springframework.data.domain.Pageable;
 import java.util.List;
@@ -53,8 +54,8 @@ public class UserServiceImplementation implements UserService {
     }
 
     @Override
-    public Page<UserModel> findAll(Pageable pageable) {
-        return userRepository.findAll(pageable);
+    public Page<UserModel> findAll(Specification<UserModel> spec,  Pageable pageable) {
+        return userRepository.findAll(spec, pageable);
     }
 
 }
