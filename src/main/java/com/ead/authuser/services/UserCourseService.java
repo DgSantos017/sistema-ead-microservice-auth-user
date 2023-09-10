94baa21385944fa9206b23c24dea2853b725d9eb
Diogo <diogodevweb@gmail.com>
2023-09-10 00:35:20 -0400

    feat: add subscription

--- src/main/java/com/ead/authuser/services/UserCourseService.java
+++ src/main/java/com/ead/authuser/services/UserCourseService.java
@@ -1,11 +1,11 @@
-package com.ead.authuser.repositories;
+package com.ead.authuser.services;
 
 import com.ead.authuser.models.UserCourseModel;
 import com.ead.authuser.models.UserModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.UUID;
 
-public interface UserCourseRepository extends JpaRepository<UserCourseModel, UUID> {
+public interface UserCourseService {
     boolean existsByUserAndCourseId(UserModel userModel, UUID courseId);
+
+    UserCourseModel save(UserCourseModel userCourseModel);
 }
